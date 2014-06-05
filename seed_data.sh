e14be1fb170b52251777fe3f2949ee5509cad68b
Wyatt Johnston <wyjohnston@gmail.com>
2014-06-05 17:00:16 -0400

    Add seed data

---
+++ seed_data.sh
@@ -0,0 +1,11 @@
+curl -d 'name=Fatty+Daddy&address=310+9th+St+New+York%2C+NY+11215&latitude=40.669169&longitude=-73.986308&rackAmount=2&createdBy=NYCDOT' http://localhost:8080/api/lockups/
+
+curl -d 'name=Underhill+Playground&address=338+Prospect+Pl+Brooklyn%2C+NY+11238&latitude=40.676771&longitude=-73.966119&rackAmount=1&createdBy=NYCDOT' http://localhost:8080/api/lockups/
+
+curl -d 'name=Brooklyn+Boulders&address=575+Degraw+St+New+York%2C+NY+11217&latitude=40.679501&longitude=-73.984279&rackAmount=1&createdBy=NYCDOT' http://localhost:8080/api/lockups/
+
+curl -d 'name=Old+Stone+House&address=329+3rd+St+Brooklyn%2C+NY+11215&latitude=40.673394&longitude=-73.984467&rackAmount=1&createdBy=NYCDOT' http://localhost:8080/api/lockups/
+
+curl -d 'name=Pavilion+Theater&address=188+Prospect+Park+West+Brooklyn%2C+NY+11215&latitude=40.661468&longitude=-73.979537&rackAmount=1&createdBy=NYCDOT' http://localhost:8080/api/lockups/
+
+echo "DB seeded"
