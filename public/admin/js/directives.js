72a08b4fa41ea1f8fcb884961fdc134a612e0e37
Wyatt Johnston <wyjohnston@gmail.com>
2014-07-02 16:54:16 -0400

    give up on places autocomplete for now

--- public/admin/js/directives.js
+++ public/admin/js/directives.js
@@ -1,1 +1,19 @@
-angular.module('adminPortal.directives', []);
+angular.module('adminPortal.directives', [])
+
+.directive('gpAutocomplete', function() {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function($scope, element) {
+      var autocomplete = new google.maps.places.Autocomplete(element[0]);
+      google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        var place = autocomplete.getPlace();
+        if (place.geometry) {
+          // map.panTo(place.geometry.location);
+          $scope.map.center = place.geometry.location;
+          $scope.map.zoom = 15;
+        }
+      });
+    }
+  };
+});
