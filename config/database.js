ffd338e7db46f982eb5c4d70fa5dce1cd52ff105
Wyatt Johnston <wyjohnston@gmail.com>
2014-07-05 16:14:22 -0400

    Switch DB to mongolab

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
   // 'url': 'mongodb://localhost:lockup-api'
-  'url': process.env.MONGO_URL
+  // 'url': process.env.MONGO_URL
+  'url': 'mongodb://wpj:mizunoie@ds027489.mongolab.com:27489/youlock'
 };
