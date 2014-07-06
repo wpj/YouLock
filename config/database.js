8b98a7c1a6c1021d7cffc3e2bd20c313f96cbe89
Wyatt Johnston <wyjohnston@gmail.com>
2014-07-05 22:32:23 -0400

    Config DB env var

--- config/database.js
+++ config/database.js
@@ -1,5 +1,3 @@
 module.exports = {
-  // 'url': 'mongodb://localhost:lockup-api'
-  // 'url': process.env.MONGO_URL
-  'url': 'mongodb://wpj:mizunoie@ds027489.mongolab.com:27489/youlock'
+  'url': process.env.DATABASE_URL
 };
