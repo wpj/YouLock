930f4c0a713e90cbfad19524d5d1ffe9928d2dbb
Wyatt Johnston <wyjohnston@gmail.com>
2014-07-07 11:51:33 -0400

    Remove close keyboard altogether

--- front-end/js/controllers.js
+++ front-end/js/controllers.js
@@ -359,7 +359,7 @@ angular.module('controllers', [])
       });
 
       Lockup.geocode($scope.searchText).then(function(data) {
-        if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) $cordovaKeyboard.close();
+        // if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) $cordovaKeyboard.close();
         $ionicLoading.hide();
         $scope.map.center = { latitude: data[0].geometry.location.k, longitude: data[0].geometry.location.A };
         // send geocoded address to server for analytics
