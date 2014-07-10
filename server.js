5941376a386112aad716a339b22ac9c2a25f1d6d
Wyatt Johnston <wyjohnston@gmail.com>
2014-07-10 19:11:15 -0400

    Serve 404 page for undefined routes

--- server.js
+++ server.js
@@ -80,6 +80,20 @@ app.use('/auth', authRouter);
 app.use('/portal', dataRouter);
 app.use('/admin', adminRouter);
 
+// 404
+app.use(function(req, res, next){
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) return res.render(__dirname + '/public/404.ejs', { url: req.url });
+
+  // respond with json
+  if (req.accepts('json')) return res.send({ error: 'Not found' });
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
+
 
 // start server
 app.listen(port);
